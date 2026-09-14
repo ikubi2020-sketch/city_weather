f710afe6e6730cdf36bf8a0c4822dffe2e4f7032
yehonatan koobi <ikubi2020@gmail.com>
2026-09-14 12:04:28 +0300

    added protected route

--- frontEnd/src/App.tsx
+++ frontEnd/src/App.tsx
@@ -1,8 +1,12 @@
 import './App.css'
 import {Routes ,Route} from "react-router" 
-// import { ProtectedRoute } from './componnents/protectedRoute/protectedRoute'
-// import Enter from './componnents/enter/Enter'
+import { ProtectedRoute } from './componnents/protectedRoute/protectedRoute'
+import Enter from './componnents/enter/Enter'
+import DashBoard from './componnents/dashboard/Dashbaord'
+import Favorites from './componnents/favorites/Favorites'
+import Compare from './componnents/compare/compare'
 import LayOut from './componnents/layout/layOut'
+import Search from './componnents/search/Search'
 
 
 function App() {
@@ -10,12 +14,36 @@ function App() {
   return (
     <>
       <Routes>
-        <Route element={<LayOut/>}>
-          <Route path="/enter"/>
+        
+          <Route path="/enter" element={<Enter/>}/>
+
+          <Route element={<LayOut/>}>
           
-            <Route path="/" />
-            <Route />
-            <Route path='*'>400, something went wrong</Route>
+              <Route path="/dashboard" element={
+                <ProtectedRoute>
+                  <DashBoard/>                                    
+                </ProtectedRoute>
+              }/>
+            
+              <Route path="/search" element={
+                <ProtectedRoute>
+                  <Search/>
+                </ProtectedRoute>
+              }/>
+              
+              <Route path="/favorite" element={
+                <ProtectedRoute>
+                  <Favorites/>
+                </ProtectedRoute>
+              }/>
+              
+              <Route path="/compare" element={
+                <ProtectedRoute>
+                  <Compare/>
+                </ProtectedRoute>
+              }/>
+
+          <Route path='*'>400, something went wrong</Route>
 
         </Route>
       </Routes>
--- frontEnd/src/componnents/enter/Enter.tsx
+++ frontEnd/src/componnents/enter/Enter.tsx
@@ -1,18 +1,27 @@
-import React, { useState, useEffect } from 'react';
+import  { useState, type SubmitEvent } from 'react';
+import { useNavigate } from 'react-router';
 
-export default function () {
-    const [userName, setUserName] = useState(() => {
-    const saved = localStorage.getItem('userName');
-    return saved ? JSON.parse(saved) : '';
-    });    
+export default function Enter() {
+    const [userName, setUserName] = useState("")
+    const navigate = useNavigate()
+   const handelSubmit = (e : SubmitEvent<HTMLFormElement>) => {
+      e.preventDefault()
+      if(userName.trim()!==  ""){
+        localStorage.setItem("userName", userName)
+        navigate("/dashboard")
+      }
+    }
 
   return (
     <div>
-        <h1>ברוכים הבאים לאתר מזג האויור</h1>
-        <h2>על מנת לגשת לאתר הכנס שם משתמש</h2>
-        <input type="text" placeholder="השם שלך" 
-        value={userName}
-        onChange={e => setUserName(e.target.value)}/>
+        <h1>welcome to weather website</h1>
+        <h2>to access please enter user name</h2>
+        <form onSubmit={handelSubmit}>
+          <input type="text" placeholder="your name" 
+          value={userName}
+          onChange={e => setUserName(e.target.value)}/>
+          <button type='submit'>ENTER</button>
+        </form>
     </div>
   )
 }
